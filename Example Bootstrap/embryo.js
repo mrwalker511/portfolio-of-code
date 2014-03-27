708c2c1dc3cd2da23d81adaf6fe93d57928ea3dc
Matt <mrwalker511@gmail.com>
2014-03-26 21:46:18 -0500

    March 25 Version 2

--- Example Bootstrap/embryo.js
+++ Example Bootstrap/embryo.js
@@ -1,6 +1,28 @@
 
 
+$(document).ready(function(){
 
+   //Adjust height of overlay to fill screen when page loads
+   $("#fade").css("height", $(document).height());
+
+   //When the link that triggers the message is clicked fade in overlay/msgbox
+   $(".alert").click(function(){
+      $("#fade").fadeIn();
+      return false;
+   });
+
+   //When the message box is closed, fade out
+   $(".close").click(function(){
+      $("#fade").fadeOut();
+      return false;
+   });
+
+});
+
+//Adjust height of overlay to fill screen when browser gets resized
+$(window).bind("resize", function(){
+   $("#fade").css("height", $(window).height());
+});
 // For the tabbed content
 
 
